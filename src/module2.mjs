ac3fa57e918ad86e5c0288fb16de35bfa8ea8789
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-19 19:48:36 +0530

    added export and import concept !!!

---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+import  def_m1 , {b,c,d} from './module1.mjs';
+
+console.log("Imported default export variable a from module1.mjs is : ",def_m1);
+console.log("Imported variable b from module1.mjs is : ",b);
+console.log("Imported variable c from module1.mjs is : ",c);
+console.log("Imported variable d from module1.mjs is : ",d);
