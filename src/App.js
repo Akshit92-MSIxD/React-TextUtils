d6f3247d539ce7bfbd814a9c89116bb864c181d9
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-22 18:32:08 +0530

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
   return (
        <>
- <Navbar title="TextUtils" about = "About us"/>
- <Navbar title="iDiscuss" about = "About me"/>
- <Navbar  about = "About me"/> {/*  Default value for title(prop) will be taken in Navbar(component) */}
+ <Navbar title="TextUtils" about = "About me"/>
+ <TextForm heading="Enter the text to analyze below" />
 
  </>
       
