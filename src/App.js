7e2d53fb635a4e30b5fe6c887b3add5492c0e96e
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-28 19:53:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 
 function App() {
   return (
-       <>
+    <>
  <Navbar title="TextUtils" about = "About me"/>
- <TextForm heading="Enter the text to analyze below" />
-
- </>
+ {/* <TextForm heading="Enter the text to analyze below" /> */}
+       <About/>
+  </>
       
   );
 }
